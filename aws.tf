f8c6f79fdbefa0249d3a7cdfd054801ccb8b663c
Paul Thrasher <thrashr888@gmail.com>
2023-07-09 16:59:37 -0700

    lower cost

--- aws.tf
+++ aws.tf
@@ -1,7 +1,7 @@
 resource "aws_budgets_budget" "ec2" {
   name              = "budget-ec2-monthly"
   budget_type       = "COST"
-  limit_amount      = "1200"
+  limit_amount      = "20"
   limit_unit        = "USD"
   time_period_end   = "2087-06-15_00:00"
   time_period_start = "2017-07-01_00:00"
