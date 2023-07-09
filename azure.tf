d82ac7f851eb0420eb0a2bab83cefbe0e2d3a7f5
Paul Thrasher <thrashr888@gmail.com>
2023-07-09 15:53:16 -0700

    Update azure.tf

--- azure.tf
+++ azure.tf
@@ -134,11 +134,25 @@ check "check_certificate_state" {
 }
 
 # ---------------------------------
-variable "azurerm_linux_function_app_example_name" {
 
+resource "azurerm_storage_account" "example" {
+  name                     = "linuxfunctionappsa"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
-variable "azurerm_linux_function_app_example_name" {
 
+resource "azurerm_linux_function_app" "example" {
+  name                = "example-linux-function-app"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+  storage_account_name       = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+  service_plan_id            = azurerm_service_plan.example.id
+
+  site_config {}
 }
 
 data "azurerm_linux_function_app" "example" {
