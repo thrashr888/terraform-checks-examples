fec3cde01ebe896e4bea6984827a0b620137d3ab
Paul Thrasher <thrashr888@gmail.com>
2023-07-09 15:47:26 -0700

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -1,6 +1,31 @@
+resource "aws_budgets_budget" "ec2" {
+  name              = "budget-ec2-monthly"
+  budget_type       = "COST"
+  limit_amount      = "1200"
+  limit_unit        = "USD"
+  time_period_end   = "2087-06-15_00:00"
+  time_period_start = "2017-07-01_00:00"
+  time_unit         = "MONTHLY"
+
+  cost_filter {
+    name = "Service"
+    values = [
+      "Amazon Elastic Compute Cloud - Compute"
+    ]
+  }
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 100
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "FORECASTED"
+    subscriber_email_addresses = ["test@example.com"]
+  }
+}
+
 check "check_budget_exceeded" {
-  data "aws_budgets_budget" "example" {
-    name = aws_budgets_budget.example.name
+  data "aws_budgets_budget" "ec2" {
+    name = aws_budgets_budget.ec2.name
   }
  
   assert {
